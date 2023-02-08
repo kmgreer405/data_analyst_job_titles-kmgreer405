2c3bfeb7b9f57c28552a2f91d53afea5d3c33e08
Kevin Greer <kmgreer405@hotmail.com>
2023-02-07 21:10:01 -0500

    Through question 8

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -54,6 +54,11 @@ FROM data_analyst_jobs;
 --881 Titles
 
 -- 8.	How many unique job titles are there for California companies?
+SELECT COUNT(DISTINCT(title))
+FROM data_analyst_jobs
+WHERE location = 'CA';
+
+--230 Titles
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
