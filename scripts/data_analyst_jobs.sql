20bd87c7369115726f59c1c0ec8f8a0eb44bc085
Kevin Greer <kmgreer405@hotmail.com>
2023-02-07 21:36:57 -0500

    Through question 12

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -85,6 +85,12 @@ WHERE title LIKE '%Analyst%';
 --1636 job titles
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%Analyst%'
+AND title NOT LIKE '%Analytics%';
+
+--39 titles do not contain these two words. The most common word among these titles is "data"
 
 -- **BONUS:**
 -- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
